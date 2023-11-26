c6cdcc1b8e31b6bdfe93002883d341f86eab23a2
Ricardo Fonseca <ricardofsdomene@icloud.com>
2023-11-25 23:14:06 -0300

    Jorneys Header

--- screens/Journeys/index.tsx
+++ screens/Journeys/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
 import * as Progress from "react-native-progress";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { ContentLabel } from "../../components/ContentLabel";
+import { useTheme } from "../../contexts/theme.context";
+import { authScreenProp } from "../../App";
+import { primaryBackgroundColor, primaryTextColor } from "../../utils/style";
+import { useNavigation } from "@react-navigation/native";
 
 const missions: {
   title: string;
@@ -31,97 +43,158 @@ const missions: {
 ];
 
 const JourneysScreen = () => {
-  return (
-    <ScrollView style={{ flex: 1, backgroundColor: "#000", padding: 20 }}>
+  const navigation = useNavigation<authScreenProp>();
+
+  const { theme } = useTheme();
+
+  const Header = () => {
+    return (
       <View
         style={{
+          display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
           alignItems: "center",
-          marginBottom: 20,
+          paddingRight: 20,
+          paddingBottom: 15,
         }}
       >
-        <Text style={{ color: "#fff", fontSize: 24 }}>Challenge</Text>
-        <TouchableOpacity>
-          <Ionicons name="ellipsis-horizontal" size={24} color="#fff" />
-        </TouchableOpacity>
-      </View>
-
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-around",
-          marginBottom: 20,
-        }}
-      >
-        <TouchableOpacity
-          style={{ backgroundColor: "#6448FE", borderRadius: 20, padding: 10 }}
-        >
-          <Text style={{ color: "#fff", fontSize: 16 }}>Target</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            backgroundColor: "transparent",
-            borderRadius: 20,
-            padding: 10,
-          }}
-        >
-          <Text style={{ color: "#fff", fontSize: 16 }}>Badges</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 20,
-        }}
-      >
-        <Text style={{ color: "#fff", fontSize: 20 }}>Daily Missions</Text>
-        <TouchableOpacity>
-          <Ionicons name="chevron-forward" size={24} color="#fff" />
-        </TouchableOpacity>
-      </View>
-
-      {missions.map((mission, index) => (
+        <ContentLabel theme={theme} title="Jornadas" />
         <View
-          key={index}
           style={{
-            backgroundColor: "#1c1c1e",
-            borderRadius: 10,
-            padding: 16,
+            display: "flex",
             flexDirection: "row",
             alignItems: "center",
-            marginBottom: 16,
           }}
         >
-          <MaterialCommunityIcons
-            name={mission.icon}
-            size={24}
-            color={mission.iconColor}
-            style={{ marginRight: 8 }}
-          />
-          <View style={{ flex: 1 }}>
-            <Text style={{ color: "#fff", fontSize: 16, marginBottom: 4 }}>
-              {mission.title}
-            </Text>
-            <Progress.Bar
-              progress={mission.progress}
-              width={null}
-              color={mission.iconColor}
-              unfilledColor="rgba(255,255,255,0.5)"
-              borderWidth={0}
-              borderRadius={5}
-              style={{ height: 10 }}
+          {/* <TouchableOpacity
+            onPress={() => {
+              navigation.removeListener;
+              navigation.navigate("Settings");
+            }}
+            style={{
+              marginLeft: 10,
+            }}
+          >
+            <Ionicons
+              name="ios-settings-outline"
+              color={primaryTextColor(theme)}
+              size={30}
             />
-          </View>
-          <Text style={{ color: "#fff", marginLeft: 8 }}>
-            {Math.floor(mission.progress * 100)}%
-          </Text>
+          </TouchableOpacity> */}
         </View>
-      ))}
-    </ScrollView>
+      </View>
+    );
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: primaryBackgroundColor(theme),
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <SafeAreaView>
+        <Header />
+        <ScrollView
+          style={{
+            backgroundColor: "#000",
+            marginTop: 10,
+            paddingHorizontal: 20,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              marginBottom: 20,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#6448FE",
+                borderRadius: 20,
+                padding: 10,
+                width: Dimensions.get("window").width / 2 - 20,
+              }}
+            >
+              <Text style={{ color: "#fff", fontSize: 16 }}>Target</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "transparent",
+                borderRadius: 20,
+                padding: 10,
+                width: Dimensions.get("window").width / 2 - 20,
+              }}
+            >
+              <Text style={{ color: "#fff", fontSize: 16 }}>Badges</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: 20,
+            }}
+          >
+            <Text style={{ color: "#fff", fontSize: 20 }}>Daily Missions</Text>
+            <TouchableOpacity>
+              <Ionicons name="chevron-forward" size={24} color="#fff" />
+            </TouchableOpacity>
+          </View>
+
+          {missions.map((mission, index) => (
+            <View
+              key={index}
+              style={{
+                backgroundColor: "#1c1c1e",
+                borderRadius: 10,
+                padding: 16,
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: 16,
+              }}
+            >
+              <MaterialCommunityIcons
+                name={mission.icon}
+                size={24}
+                color={mission.iconColor}
+                style={{ marginRight: 8 }}
+              />
+              <View style={{ flex: 1 }}>
+                <Text style={{ color: "#fff", fontSize: 16, marginBottom: 4 }}>
+                  {mission.title}
+                </Text>
+                <Progress.Bar
+                  progress={mission.progress}
+                  width={null}
+                  color={mission.iconColor}
+                  unfilledColor="rgba(255,255,255,0.5)"
+                  borderWidth={0}
+                  borderRadius={5}
+                  style={{ height: 10 }}
+                />
+              </View>
+              <Text style={{ color: "#fff", marginLeft: 8 }}>
+                {Math.floor(mission.progress * 100)}%
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 };
 
